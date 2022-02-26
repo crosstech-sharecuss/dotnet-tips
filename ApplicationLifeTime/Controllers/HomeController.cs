f25b4b750838c9b97d4c1296a9f1802d344f271e
Emin YILDIZ <eyeminyildiz@outlook.com>
2022-02-26 22:39:49 +0300

    Created ApplicationLifeTime Project

---
+++ ApplicationLifeTime/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+
+namespace ApplicationLifeTime.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class HomeController : ControllerBase
+	{
+		private readonly IHostApplicationLifetime _applicationLifetime;
+
+		public HomeController(IHostApplicationLifetime applicationLifetime)
+		{
+			_applicationLifetime = applicationLifetime;
+		}
+
+		[HttpGet]
+		public IActionResult Get()
+		{
+			_applicationLifetime.StopApplication();
+
+			return Ok("Endpoint worked...");
+		}
+	}
+}
---
+++ ApplicationLifeTime/Startup.cs
@@ -0,0 +1,50 @@
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace ApplicationLifeTime
+{
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		public IConfiguration Configuration { get; }
+
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddControllers();
+			services.AddSwaggerGen(c =>
+			{
+				c.SwaggerDoc("v1", new OpenApiInfo { Title = "ApplicationLifeTime", Version = "v1" });
+			});
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+				app.UseSwagger();
+				app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ApplicationLifeTime v1"));
+			}
+
+			app.UseRouting();
+
+			app.UseAuthorization();
+
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapControllers();
+			});
+		}
+	}
+}
